209c5db973f36dbb192ae872f957f813be2a08a5
Reda <reda.moukarrab@gmail.com>
2019-06-19 22:43:32 -0400

    Lesson 6 added

--- Lesson_5/Lesson_5.cpp
+++ Lesson_5/Lesson_5.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Size of int" << sizeof(int)<<endl;
+	std::cout << "Size of long" << sizeof(long) << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
