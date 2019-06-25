c3950617fce400b4cbfab4890bfa80a3d76e076c
Reda <reda.moukarrab@gmail.com>
2019-06-24 22:48:54 -0400

    Lesson 6 , Vector , for loop

--- Lesson_6/Lesson_6.cpp
+++ Lesson_6/Lesson_6.cpp
@@ -2,8 +2,41 @@
 //
 
 #include <iostream>
-
+#include<vector>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int somenums[] = { 1,101,-1,40,2040 };
+	int mynumber = 0;
+	vector<int> myvector;
+
+
+	std::cout << "Hello World!\n" << endl;
+
+	/*Range-based for loop*/
+	for (int aNum : somenums)
+	{
+		cout << "The array elements are " << aNum << endl;
+	}
+
+	/*Range-based for loop with auto */
+	for (auto aNum : somenums)
+	{
+		cout << "The array elements are " << aNum << endl;
+	}
+
+
+	do {
+		/*vector input*/
+		cout << "Please enter number or -1 to terminate" << endl;
+		cin >> mynumber;
+		myvector.push_back(mynumber);
+	} while (mynumber != -1);
+
+	/*Range based for loop */
+	for (int i=0 ; i<(sizeof(myvector)/sizeof(int));i++)
+	{
+		cout << "number of id" << i <<" is :"<<myvector[i]<< endl;
+	}
 }
+
